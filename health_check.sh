4b2e13eedaf896eabca85ddc87dca59daf20e2c5
Robin <rj.dotson2211@gmail.com>
2026-03-05 11:55:19 -0500

    Added user security test and log rotation logic

--- health_check.sh
+++ health_check.sh
@@ -15,3 +15,7 @@ df -h | grep '^/dev/'
 echo ""
 echo "Network Info:"
 ip addr show eth0 | grep inet
+LINE_COUNT=$(wc -l < system_logs.txt)
+if [$LINE_COUNT -gt 10 ]; then
+	echo "--- Log too long. Rotating... ---" > system_logs.txt
+fi
